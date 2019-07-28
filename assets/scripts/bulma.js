1bf4f14f36decb64d14a39b7cc06230450dffcd1
jaypalexa <jpalexa@gmail.com>
2019-07-28 11:06:28 +0100

    Toggle function tweaks

--- assets/scripts/bulma.js
+++ assets/scripts/bulma.js
@@ -1,20 +1,20 @@
-//document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
   // Get "navbar-burger" element
   const $navbarBurger = document.querySelector('.navbar-burger');
 
-  console.log('$navbarBurger', $navbarBurger);
+  // console.log('$navbarBurger', $navbarBurger);
 
   // Add a click event on it
   $navbarBurger.addEventListener('click', () => {
 
-    console.log('$navbarBurger click', 'click');
+    // console.log('$navbarBurger click', 'click');
 
     // Get the target from the "data-target" attribute
     const target = $navbarBurger.dataset.target;
     const $target = document.getElementById(target);
 
-    console.log('$target', $target);
+    // console.log('$target', $target);
 
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $navbarBurger.classList.toggle('is-active');
@@ -22,4 +22,4 @@
 
   });
 
-//});
+});
