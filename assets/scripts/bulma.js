47491325e3b98b2843718471b21980fc9fdfc536
jaypalexa <jpalexa@gmail.com>
2019-07-28 10:52:34 +0100

    Toggle debugging

--- assets/scripts/bulma.js
+++ assets/scripts/bulma.js
@@ -3,17 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
   // Get "navbar-burger" element
   const $navbarBurger = document.querySelector('.navbar-burger');
 
-    // Add a click event on it
-    $navbarBurger.addEventListener('click', () => {
+  console.log('$navbarBurger', $navbarBurger);
 
-      // Get the target from the "data-target" attribute
-      const target = $navbarBurger.dataset.target;
-      const $target = document.getElementById(target);
+  // Add a click event on it
+  $navbarBurger.addEventListener('click', () => {
 
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $navbarBurger.classList.toggle('is-active');
-      $target.classList.toggle('is-active');
+    console.log('$navbarBurger click', 'click');
 
-    });
+    // Get the target from the "data-target" attribute
+    const target = $navbarBurger.dataset.target;
+    const $target = document.getElementById(target);
+
+    console.log('$target', $target);
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $navbarBurger.classList.toggle('is-active');
+    $target.classList.toggle('is-active');
+
+  });
 
 });
